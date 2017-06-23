fa4117df4a50fbd4b2f9ce8ed289801f5b6a063e
Vinícius Ribeiro <vinicius@nanomedia.com.br>
2017-06-23 02:44:10 -0300

    Adiciona componentes e templates js

--- dev/js/app_plugins.js
+++ dev/js/app_plugins.js
@@ -1,23 +1,38 @@
 "use strict";
 
 var app_plugins = {
-    bootstrap_select: function(){
+    bootstrap_select: function() {
         if($(".bs-select").length > 0)
            $(".bs-select").selectpicker({iconBase: '', tickIcon: 'fa fa-check'});
     },
-    bootstrap_tooltip: function(){
+    bootstrap_tooltip: function() {
         $("[data-toggle='tooltip']").tooltip();
     },
-    loaded: function(){
+    statusbar: {
+        init: function() {
+            $(".app-statusbar-open, .app-statusbar-close").on("click",function () {
+                app.statusbar.open($(this).attr('href'));
+                return false;
+            });
+        },
+        open: function(id) {
+            $(".app-statusbar").hide();
+            if($(id).is(":hidden")) $(id).fadeIn();
+        }
+    },
+    search: function() {
+        $(".app-header-search").on("click",function(){
+            $(".app-header-search > input").focus();
+        });
+    },
+    loaded: function() {
         app_plugins.bootstrap_select();
         app_plugins.bootstrap_tooltip();
+        app_plugins.statusbar.init();
+        app_plugins.search();
     }
 };
 
 $(function(){
     app_plugins.loaded();
-});
-
-$(document).ready(function(){
-    app.loaded();
 });
