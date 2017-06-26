560fec45200f12f42aad80519eed917de06c4080
Vinícius Ribeiro <vinicius@nanomedia.com.br>
2017-06-26 16:07:49 -0300

    Adiciona tradução do BS Select

--- dev/js/vendor/bootstrap-select/i18n/defaults-pt_BR.js
+++ dev/js/vendor/bootstrap-select/i18n/defaults-pt_BR.js
@@ -6,10 +6,12 @@
  */
 (function ($) {
   $.fn.selectpicker.defaults = {
-    noneSelectedText: 'Nada selecionado',
+    noneSelectedText: 'Filtrar por linguagem',
     noneResultsText: 'Nada encontrado contendo {0}',
     countSelectedText: 'Selecionado {0} de {1}',
     maxOptionsText: ['Limite excedido (máx. {n} {var})', 'Limite do grupo excedido (máx. {n} {var})', ['itens', 'item']],
-    multipleSeparator: ', '
+    multipleSeparator: ', ',
+    selectAllText: 'Marcar Todos',
+    deselectAllText: 'Desmarcar Todos',
   };
 })(jQuery);
