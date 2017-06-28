908878653632d826fbadeafb6c58d4dab0b69152
Vinícius Ribeiro <vinicius@nanomedia.com.br>
2017-06-28 17:27:15 -0300

    Refactor JS

--- dev/js/app.js
+++ dev/js/app.js
@@ -4,35 +4,36 @@
     // Cache DOM elements
     var DOMCache = {
         cards: {
-            wrapper: $('#appCardList'),
-            vcard: $('[data-card-template]').html().trim(),
+            wrapper: $( '#appCardList' ),
+            vcard: $( '[data-card-template]' ).html().trim(),
         },
         profile: {
-            wrapper: $('#userProfileWrapper'),
-            info: $('[data-user-template]').html().trim(),
+            wrapper: $( '#userProfileWrapper' ),
+            info: $( '[data-user-template]' ).html().trim(),
         },
         widgets: {
-            widgetContainer: $('#limitInformer'),
-            rateValue: $('<div id="rateValue" class="intval">'),
-            rateText: $('<div id="rateText" class="subtitle">Limite da API</div>'),
+            widgetContainer: $( '#limitInformer' ),
+            progressBarContainer: $( "#limitProgressBar" ),
+            rateValue: $( '<div id="rateValue" class="intval">' ),
+            rateText: $( '<div id="rateText" class="subtitle">Limite da API</div>' ),
         },
         selects: {
-            orderBy: $('#filterSelect'),
-            languageSelect: $('#languageSelect'),
-            option: $('<option></option>'),
+            orderBy: $( '#filterSelect' ),
+            languageSelect: $( '#languageSelect' ),
+            option: $( '<option></option>' ),
         },
         loadMore: {
-            loadMoreBtn: $('<button type="button" class="btn btn-large btn-info btn-rounded">Carregar Mais</button>'),
-            loadMoreWrapper: $('#loadMoreWrapper'),
+            loadMoreBtn: $( '<button type="button" class="btn btn-large btn-info btn-rounded">Carregar Mais</button>' ),
+            loadMoreWrapper: $( '#loadMoreWrapper' ),
         },
         alerts: {
-            warning: $('[data-alert-warning-template]').html(),
-            danger: $('[data-alert-danger-template]').html(),
-            userDanger: $('[data-user-danger-template]').html(),
-            noLoadMore: $('<h5 class="text-center">Não há mais repositórios a carregar</h5>'),
+            warning: $( '[data-alert-warning-template]' ).html(),
+            danger: $( '[data-alert-danger-template]' ).html(),
+            userDanger: $( '[data-user-danger-template]' ).html(),
+            noLoadMore: $( '<h5 class="text-center">Não há mais repositórios a carregar</h5>' ),
         },
-        searchBar: $('#searchBar'),
-        repoCounter: $('#repoCounter'),
+        searchBar: $( '#searchBar' ),
+        repoCounter: $( '#repoCounter' ),
     };
 
     // Cache Objects
@@ -45,18 +46,22 @@
     // Application helpers
     var helpers = {
         countObjectProperties: function( obj ) {
-            var count = 0;
-            for ( var property in obj ) {
-                if ( Object.prototype.hasOwnProperty.call(obj, property) ) {
-                    count++;
+            if ( obj != null ) {
+                var countProperties = 0;
+                for ( var property in obj ) {
+                    if ( Object.prototype.hasOwnProperty.call(obj, property) ) {
+                        countProperties++;
+                    }
                 }
+                return countProperties;
             }
-            return count;
         },
         removeDuplicateObj: function( arrLanguages ) {
-            return arrLanguages.filter(function( elem, pos, arr ) {
-                return arr.indexOf( elem ) == pos;
-            });
+            if ( Array.isArray(arrLanguages) ) {
+                return arrLanguages.filter(function( elem, pos, arr ) {
+                    return arr.indexOf( elem ) == pos;
+                });
+            }
         },
         filtered: function( obj, filters ) {
             if ( obj != null ) {
@@ -86,19 +91,18 @@
         usersEndpoint: 'https://api.github.com/users/',
         rateLimit: 'https://api.github.com/rate_limit',
         getRateLimit: function() {
-            $.get( this.rateLimit, function( data ) {
-            })
+            $.get( this.rateLimit, function( data ) {})
             .done(function( data ) {
                render.limitInformer( data.rate );
             })
         },
         getStarredRepositories: function( usersEndpoint, username, currentPage ) {
-            render.placeholder('render');
+            render.placeholder('renderPlaceholder');
             $.ajax({
                 url: githubAPI.usersEndpoint + githubAPI.username +'/starred?page='+ githubAPI.currentPage,
                 type: 'GET',
                 success: function( data ) {
-                    render.placeholder('clear');
+                    render.placeholder('clearPlaceholder');
                     githubAPI.getRateLimit();
                     githubAPI.getProfileInfo();
 
@@ -126,11 +130,12 @@
                     githubAPI.getRateLimit();
 
                     if ( data.status == 403 ) {
+                        render.placeholder('clearPlaceholder');
                         app_plugins.statusbar.open('#danger');
                     }
 
                     if ( data.status == 404 ) {
-                        render.placeholder('clear');
+                        render.placeholder('clearPlaceholder');
                         app_plugins.statusbar.open('#userDanger');
                     }
                 }
@@ -151,7 +156,7 @@
     // Render Components
     var render = {
         limitInformer: function( apiData ) {
-            if ( apiData != null || apiData != undefinied ) {
+            if ( apiData != null ) {
                 var apidata    = apiData;
                 var remaining  = apiData.remaining;
                 var limit      = apiData.limit;
@@ -159,32 +164,34 @@
 
                 DOMCache.widgets.rateValue.text( remaining + ' / ' + limit) ;
                 DOMCache.widgets.widgetContainer.html();
-                DOMCache.widgets.widgetContainer.append( [ DOMCache.widgets.rateValue, DOMCache.widgets.rateText] );
+                DOMCache.widgets.widgetContainer.append([
+                    DOMCache.widgets.rateValue,
+                    DOMCache.widgets.rateText
+                ]);
 
                 render.limitProgressBar( percentage );
             }
         },
         limitProgressBar: function( percentage ) {
             var percentage = percentage;
-            var $progressContainer = $( "#limitProgressBar" );
 
-            $progressContainer
+            DOMCache.widgets.progressBarContainer
             .css({ "width": percentage +"%" })
             .text(percentage + '%');
 
             if ( percentage >= 50 ) {
-                $progressContainer
+                DOMCache.widgets.progressBarContainer
                 .removeClass('progress-bar-danger')
                 .removeClass('progress-bar-warning')
                 .addClass('progress-bar-success');
             }
             if ( percentage < 50 ) {
-                $progressContainer
+                DOMCache.widgets.progressBarContainer
                 .removeClass('progress-bar-success')
                 .addClass('progress-bar-warning');
             }
             if ( percentage < 25 ) {
-                $progressContainer
+                DOMCache.widgets.progressBarContainer
                 .removeClass('progress-bar-warning')
                 .addClass('progress-bar-danger');
                 app_plugins.statusbar.open('#warning');
@@ -200,57 +207,66 @@
             var repositoriesCount = helpers.countObjectProperties( repositoriesData );
 
             if ( repositoriesCount != 0 ) {
-                repositoriesData.map( function(obj) {
-                    var $card = $(DOMCache.cards.vcard);
-                    var created = moment(obj.created_at).format('DD\/MM\/YYYY');
-                    var pushed = moment(obj.pushed_at).format('DD\/MM\/YYYY');
+                repositoriesData.map(function( obj ) {
+                    var $card      = $( DOMCache.cards.vcard );
+                    var created    = moment( obj.created_at ).format( 'DD\/MM\/YYYY' );
+                    var pushed     = moment( obj.pushed_at ).format( 'DD\/MM\/YYYY' );
                     var publicRepo = "Público";
-                    var sizeMB = parseFloat( obj.size / 1024 ).toFixed(2);
-                    var language = obj.language;
+                    var sizeMB     = parseFloat( obj.size / 1024 ).toFixed(2);
+                    var language   = obj.language;
                     var languageClass;
-                    if ( language != null ) var languageClass = language.toLowerCase();
-                    if ( obj.private ) var publicRepo = "Privado";
 
-                    $card.find('[data-card-name]').html(obj.name);
-                    $card.find('[data-card-name]').attr('href', obj.html_url);
-                    $card.find('[data-card-user]').html('by ' + obj.owner.login);
-                    $card.find('[data-card-image]').attr('src', obj.owner.avatar_url);
-                    $card.find('[data-card-repo-link]').attr('href', githubAPI.githubUrl + obj.owner.login + '?tab=repositories');
-                    $card.find('[data-card-profile-link]').attr('href', githubAPI.githubUrl + obj.owner.login);
-                    $card.find('[data-card-stars]').html(obj.stargazers_count);
-                    $card.find('[data-card-issues]').html(obj.open_issues);
-                    $card.find('[data-card-watchers]').html(obj.watchers_count);
-                    $card.find('[data-card-created]').html(created);
-                    $card.find('[data-card-pushed]').html(pushed);
-                    $card.find('[data-card-public]').html(publicRepo);
-                    $card.find('[data-card-size]').html(sizeMB + ' MB');
-                    $card.find('[data-card-language]').html(language);
+                    if ( language != null )
+                        var languageClass = language.toLowerCase().trim();
+                    if ( obj.private )
+                        var publicRepo = "Privado";
+
+                    $card.find( '[data-card-name]' ).html( obj.name );
+                    $card.find( '[data-card-name]' ).attr( 'href', obj.html_url );
+                    $card.find( '[data-card-user]' ).html( 'by ' + obj.owner.login );
+                    $card.find( '[data-card-image]' ).attr( 'src', obj.owner.avatar_url );
+                    $card.find( '[data-card-repo-link]' ).attr( 'href', githubAPI.githubUrl + obj.owner.login + '?tab=repositories' );
+                    $card.find( '[data-card-profile-link]' ).attr( 'href', githubAPI.githubUrl + obj.owner.login );
+                    $card.find( '[data-card-stars]' ).html( obj.stargazers_count );
+                    $card.find( '[data-card-issues]' ).html( obj.open_issues );
+                    $card.find( '[data-card-watchers]' ).html( obj.watchers_count );
+                    $card.find( '[data-card-created]' ).html( created );
+                    $card.find( '[data-card-pushed]' ).html( pushed );
+                    $card.find( '[data-card-public]' ).html( publicRepo );
+                    $card.find( '[data-card-size]' ).html( sizeMB + ' MB' );
+                    $card.find( '[data-card-language]' ).html( language );
+
                     if ( languageClass != null ) {
-                        $card.find('[data-card-language]').addClass(languageClass);
+                        $card
+                        .find( '[data-card-language]' ).addClass( languageClass );
                     }
-                    DOMCache.cards.wrapper.append($card);
+
+                    DOMCache.cards.wrapper.append( $card );
                     render.repositoriesValue();
                 })
             }
         },
-        userProfile: function( obj ) {
-            DOMCache.profile.wrapper.html('');
-            var $userProfile = $(DOMCache.profile.info);
-            $userProfile.find('[data-user-name]').html(obj.name);
-            $userProfile.find('[data-company-name]').html(obj.company);
-            $userProfile.find('[data-user-image]').attr('src', obj.avatar_url);
-            $userProfile.find('[data-user-profile-link]').attr('href', githubAPI.githubUrl + obj.login);
-            $userProfile.find('[data-user-repos-link]').attr('href', githubAPI.githubUrl + obj.login + '?tab=repositories');
-            $userProfile.find('[data-user-followrs-link]').attr('href', githubAPI.githubUrl + obj.login + '?tab=followers');
+        userProfile: function( apiData ) {
+            if ( apiData != null ) {
+                DOMCache.profile.wrapper.html('');
 
-            DOMCache.profile.wrapper.append($userProfile);
+                var $userProfile = $( DOMCache.profile.info );
+                $userProfile.find( '[data-user-name]' ).html( apiData.name );
+                $userProfile.find( '[data-company-name]' ).html( apiData.company );
+                $userProfile.find( '[data-user-image]' ).attr( 'src', apiData.avatar_url );
+                $userProfile.find( '[data-user-profile-link]' ).attr( 'href', githubAPI.githubUrl + apiData.login );
+                $userProfile.find( '[data-user-repos-link]' ).attr( 'href', githubAPI.githubUrl + apiData.login + '?tab=repositories' );
+                $userProfile.find( '[data-user-followrs-link]' ).attr( 'href', githubAPI.githubUrl + apiData.login + '?tab=followers' );
+
+                DOMCache.profile.wrapper.append( $userProfile );
+            }
         },
         languages: function( obj ) {
-            if ( obj.language !== null ) {
+            if ( obj !== null ) {
                 cachedObj.languagesObj = obj.forEach( function(obj) {
-                    if ( obj.language !== null ) cachedObj.languagesArr.push(obj.language);
+                    if ( obj.language !== null ) cachedObj.languagesArr.push( obj.language );
                 });
-                var filteredLanguages = helpers.removeDuplicateObj( cachedObj.languagesArr).sort();
+                var filteredLanguages = helpers.removeDuplicateObj( cachedObj.languagesArr ).sort();
                 DOMCache.selects.languageSelect.html('');
                 filteredLanguages.map( function( obj ) {
                     DOMCache.selects.languageSelect.append( '<option value="'+ obj +'">' + obj + '</option>' );
@@ -259,64 +275,73 @@
             }
         },
         loadMore: function( boolean ) {
-            if ( boolean == true ) {
-                DOMCache.loadMore.loadMoreBtn.show();
-                DOMCache.alerts.noLoadMore.hide();
-                DOMCache.loadMore.loadMoreWrapper.append( DOMCache.loadMore.loadMoreBtn );
-            }
-            if ( boolean == false ) {
-                DOMCache.loadMore.loadMoreBtn.hide();
-                DOMCache.alerts.noLoadMore.show();
-                DOMCache.loadMore.loadMoreWrapper.append( DOMCache.alerts.noLoadMore );
+            if ( typeof boolean ) {
+                if ( boolean ) {
+                    DOMCache.loadMore.loadMoreBtn.show();
+                    DOMCache.alerts.noLoadMore.hide();
+                    DOMCache.loadMore.loadMoreWrapper.append( DOMCache.loadMore.loadMoreBtn );
+                } else {
+                    DOMCache.loadMore.loadMoreBtn.hide();
+                    DOMCache.alerts.noLoadMore.show();
+                    DOMCache.loadMore.loadMoreWrapper.append( DOMCache.alerts.noLoadMore );
+                }
             }
         },
         placeholder: function( value ) {
-            if ( value == "render" ) {
+            if ( value === 'renderPlaceholder' ) {
                 for ( var i = 0; i < 9; i++ ) {
-                    var $loadingCard = $('<div class="col-md-4 item featured result-item"><div class="timeline-item"><div class="animated-background"><div class="background-masker content-top"></div>');
+                    var $loadingCard = $( '<div class="col-md-4 item featured result-item"><div class="timeline-item"><div class="animated-background"><div class="background-masker content-top"></div>' );
                     DOMCache.cards.wrapper.append( $loadingCard );
                 }
             }
-            else if ( value == "clear" ) {
-                DOMCache.cards.wrapper.find('.result-item').remove();
+            else if ( value === 'clearPlaceholder' ) {
+                DOMCache.cards.wrapper.find( '.result-item' ).remove();
             }
             else {
-                DOMCache.cards.wrapper.find('.result-item').remove();
+                DOMCache.cards.wrapper.find( '.result-item' ).remove();
             }
         },
         repositoriesValue: function() {
-            var DOMLenght = ($('[data-card]').length) -1;
+            var DOMLenght = $( '[data-card]' ).length -1;
             if ( DOMLenght == 0 ) return;
-            if ( DOMLenght == 1 ) DOMCache.repoCounter.text('Exibindo ' + DOMLenght + ' repositório.');
-            if ( DOMLenght > 1 ) DOMCache.repoCounter.text('Exibindo ' + DOMLenght + ' repositórios');
+            if ( DOMLenght == 1 ) DOMCache.repoCounter.text( 'Exibindo ' + DOMLenght + ' repositório.' );
+            if ( DOMLenght > 1 ) DOMCache.repoCounter.text( 'Exibindo ' + DOMLenght + ' repositórios' );
         }
     };
 
     // OrderBy - using COC
     var orderBy = {
         ascending: function( obj ) {
-            obj.sort( function( a, b ) {
-                return 2 * ( a.name.toLowerCase() > b.name.toLowerCase() ) - 1;
-            });
-            return render.repositoriesCards(obj, true);
+            if ( obj != null ) {
+                obj.sort( function( a, b ) {
+                    return 2 * ( a.name.toLowerCase() > b.name.toLowerCase() ) - 1;
+                });
+                return render.repositoriesCards( obj, true );
+            }
         },
         openIssues: function( obj ) {
-            obj.sort( function( a, b ) {
-                return b.open_issues - a.open_issues;
-            });
-            return render.repositoriesCards(obj, true);
+            if ( obj != null ) {
+                obj.sort( function( a, b ) {
+                    return b.open_issues - a.open_issues;
+                });
+                return render.repositoriesCards( obj, true );
+            }
         },
         stargazers: function( obj ) {
-            obj.sort( function( a, b ) {
-                return b.stargazers_count - a.stargazers_count;
-            });
-            return render.repositoriesCards(obj, true);
+            if ( obj != null ) {
+                obj.sort( function( a, b ) {
+                    return b.stargazers_count - a.stargazers_count;
+                });
+                return render.repositoriesCards( obj, true );
+            }
         },
         watchers: function( obj ) {
-            obj.sort( function( a, b ) {
-                return b.watchers - a.watchers;
-            });
-            return render.repositoriesCards(obj, true);
+            if ( obj != null ) {
+                obj.sort( function( a, b ) {
+                    return b.watchers - a.watchers;
+                });
+                return render.repositoriesCards( obj, true );
+            }
         },
     };
 
